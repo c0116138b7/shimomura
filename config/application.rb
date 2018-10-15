25d6d7fc8257db50d5b6833fafbc961026ca10ed
c0116138b7 <c0116138b7@edu.teu.ac.jp>
2018-10-15 05:42:14 +0000

    succes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Test2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     Refile.secret_key = '1803e1173e167af264f9b044e44d6d83bb49baf4dc235595ade295d106996a7078c88e831d80820d571cbc255b0b88a9636d0f0e68025f60d2430d5a06c0dc92'
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
